65cd82e476b49278e970fa5e7cc78e2caaeebd98
eric2025repo <bautista.eric@titans.easternflorida.edu>
2025-06-19 12:22:40 -0400

    added wikipedia, facebook, twitter, instagram more formatting

--- app/src/main/java/com/cop4655/isstracker/CrewActivity.kt
+++ app/src/main/java/com/cop4655/isstracker/CrewActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
@@ -13,6 +12,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.toColorInt
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.squareup.picasso.Picasso
@@ -20,7 +20,6 @@ import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
-import androidx.core.net.toUri
 
 class CrewActivity : AppCompatActivity() {
     private lateinit var iv_patch: ImageView
@@ -42,7 +41,7 @@ class CrewActivity : AppCompatActivity() {
         tv_end_date = findViewById(R.id.tv_end_date)
         val crewLayout: LinearLayout = findViewById(R.id.crew)
         val topMargin = 20F
-        val leftMargin = 305F
+        val leftMargin = 340F
 
         ExpeditionCall().getExpedition(this) { expedition ->
             // set crew header content
@@ -76,11 +75,21 @@ class CrewActivity : AppCompatActivity() {
             }
             tv_start_date.text = buildString {
                 append(getString(R.string.start_date))
-                append(epochToReadableDateWithTimeZone(expedition.expedition_start_date+(86400), "UTC").toString())
+                append(
+                    epochToReadableDateWithTimeZone(
+                        expedition.expedition_start_date + (86400),
+                        "UTC"
+                    ).toString()
+                )
             }
             tv_end_date.text = buildString {
                 append(getString(R.string.end_date))
-                append(epochToReadableDateWithTimeZone(expedition.expedition_end_date+(86400), "UTC").toString())
+                append(
+                    epochToReadableDateWithTimeZone(
+                        expedition.expedition_end_date + (86400),
+                        "UTC"
+                    ).toString()
+                )
             }
 
             // iterate through crew members
@@ -88,24 +97,33 @@ class CrewActivity : AppCompatActivity() {
                 if (person.iss) {
                     // create dynamic imageview to add to layout
                     val personLayout = RelativeLayout(this)
+                    // create elements for each person layout
                     val portraitImageView = ImageView(this)
                     val nameTextView = TextView(this)
                     val positionTextView = TextView(this)
                     val flagImageView = ImageView(this)
                     val agencyTextView = TextView(this)
                     val launchedTextView = TextView(this)
+                    val facebookImageView = ImageView(this)
+                    val twitterImageView = ImageView(this)
+                    val instagramImageView = ImageView(this)
+                    /*
+                    // generate ids for each element
                     val portraitId: Int = View.generateViewId()
                     val nameId: Int = View.generateViewId()
                     val positionId: Int = View.generateViewId()
                     val flagId: Int = View.generateViewId()
                     val agencyId: Int = View.generateViewId()
                     val launchedId: Int = View.generateViewId()
+                    // set element ids
                     portraitImageView.id = portraitId
                     nameTextView.id = nameId
                     positionTextView.id = positionId
                     flagImageView.id = flagId
                     agencyTextView.id = agencyId
                     launchedTextView.id = launchedId
+
+                     */
 //                    imageViewParam.addRule(RelativeLayout.CENTER_HORIZONTAL);
                     //imageView.layoutParams = RelativeLayout.
 
@@ -120,6 +138,7 @@ class CrewActivity : AppCompatActivity() {
                         it.setMargins(5, 5, 5, 5)
                     }
 
+                    // set element characteristics
                     personLayout.setBackgroundColor("#000080".toColorInt())
                     nameTextView.setTextColor("#FFFFFF".toColorInt())
                     nameTextView.setTypeface(null, Typeface.BOLD)
@@ -136,6 +155,7 @@ class CrewActivity : AppCompatActivity() {
                     launchedTextView.setTextColor("#FFFFFF".toColorInt())
                     launchedTextView.textSize = 17F
 
+                    // apply characteristics to person layout
                     personLayout.apply {
                         this.layoutParams = personLayoutParams
                     }
@@ -145,16 +165,25 @@ class CrewActivity : AppCompatActivity() {
                     nameTextView.y = topMargin
 
                     positionTextView.x = leftMargin
-                    positionTextView.y = topMargin + 60F
+                    positionTextView.y = topMargin + 72F
 
                     flagImageView.x = leftMargin
-                    flagImageView.y = topMargin + 111F
+                    flagImageView.y = topMargin + 145F
 
                     agencyTextView.x = leftMargin + 100F
-                    agencyTextView.y = topMargin + 117F
+                    agencyTextView.y = topMargin + 145F
 
                     launchedTextView.x = leftMargin
-                    launchedTextView.y = topMargin + 177F
+                    launchedTextView.y = topMargin + 217F
+
+                    facebookImageView.x = leftMargin + 10F
+                    facebookImageView.y = topMargin + 290F
+
+                    twitterImageView.x = leftMargin + 145F
+                    twitterImageView.y = topMargin + 290F
+
+                    instagramImageView.x = leftMargin + 300F
+                    instagramImageView.y = topMargin + 290F
 
                     // populate images and values
                     Picasso.get()
@@ -179,6 +208,51 @@ class CrewActivity : AppCompatActivity() {
                         .resize(80, 80)
                         .into(flagImageView)
 
+                    Picasso.get()
+                        .load(R.drawable.facebook_logo_secondary)
+                        .centerCrop()
+                        .resize(60, 60)
+                        .into(facebookImageView)
+
+                    Picasso.get()
+                        .load(R.drawable.logo_white)
+                        .centerCrop()
+                        .resize(60, 60)
+                        .into(twitterImageView)
+
+                    Picasso.get()
+                        .load(R.drawable.instagram_glyph_gradient)
+                        .centerCrop()
+                        .resize(60, 60)
+                        .into(instagramImageView)
+
+                    // set onclick listeners
+                    if (person.url != "") {
+                        portraitImageView.setOnClickListener {
+                            val openURL = Intent(Intent.ACTION_VIEW)
+                            openURL.data = person.url.toUri()
+                            startActivity(openURL)
+                        }
+                    }
+
+                    facebookImageView.setOnClickListener {
+                        val openURL = Intent(Intent.ACTION_VIEW)
+                        openURL.data = person.facebook.toUri()
+                        startActivity(openURL)
+                    }
+
+                    twitterImageView.setOnClickListener {
+                        val openURL = Intent(Intent.ACTION_VIEW)
+                        openURL.data = person.twitter.toUri()
+                        startActivity(openURL)
+                    }
+
+                    instagramImageView.setOnClickListener {
+                        val openURL = Intent(Intent.ACTION_VIEW)
+                        openURL.data = person.instagram.toUri()
+                        startActivity(openURL)
+                    }
+
                     // add text and images to personLayout
                     personLayout.addView(portraitImageView)
                     personLayout.addView(nameTextView)
@@ -186,6 +260,15 @@ class CrewActivity : AppCompatActivity() {
                     personLayout.addView(flagImageView)
                     personLayout.addView(agencyTextView)
                     personLayout.addView(launchedTextView)
+                    if (person.facebook != "") {
+                        personLayout.addView(facebookImageView)
+                    }
+                    if (person.twitter != "") {
+                        personLayout.addView(twitterImageView)
+                    }
+                    if (person.instagram != "") {
+                        personLayout.addView(instagramImageView)
+                    }
                     // add personLayout to crewLayout
                     crewLayout.addView(personLayout)
                 }
